9e19a760ecf9770cedb732d2229caa1cd57b5de1
Pierre Neuvial <pierre.neuvial@math.cnrs.fr>
2015-06-18 04:18:01 +0000

    minor updates

--- inst/testScripts/BNR/20150618,equicorrelation.R
+++ inst/testScripts/BNR/20150618,equicorrelation.R
@@ -12,36 +12,36 @@ alpha <- 0.2
 
 flavor <- "equi-correlated"
 
-rhos <- c(0, 0.1, 0.2, 0.3, 0.5, 0.8)
-parName <- "rho"
-params <- rhos
+rhos <- seq(from=0, to=0.9, by=0.1)
 
-lambdas <- NULL
-probs <- NULL
+nbSimu <- 10
 
-nbSimu <- 6
-res <- array(NA_real_, dim=c(length(rhos), length(methods), nbSimu))
+library("parallel")
+if (detectCores()<30) stop("This is intended to be run on a 32 CPU machine")
+resP <- mclapply(1:length(rhos), FUN=function(rr) {
+    rho <- rhos[rr]
+    print(rho)
+    
+    resRR <- mclapply(1:nbSimu, FUN=function(bb) {
+        mat <- simulateGaussianNullsFromFactorModel(m, n=B, flavor=flavor, rho=rho)
+        gammas <- numeric(0L)
+        for (mm in methods) {
+            resJ <- getJointFWERThresholds(mat, tau=mm, alpha, maxSteps=maxSteps, kMax=kMax)
+            gammas <- c(gammas, resJ$lambda)
+        }
+        gammas
+    }, mc.cores=8)
+    resRR <- do.call(cbind, resRR)
+    res[rr,,] <- resRR
+}, mc.cores=3)
+
+library("abind")
+res <- abind(resP, along=0)
 dimnames(res) <- list(rho=as.character(rhos),
                       method=methods,
                       B=as.character(1:nbSimu))
 
-library("parallel")
-for (rr in seq(along=rhos)) {
-    rho <- rhos[rr]
-    print(rho)
-
-    resRR <- mclapply(1:nbSimu, FUN=function(bb) {
-                          mat <- simulateGaussianNullsFromFactorModel(m, n=B, flavor=flavor, rho=rho)
-                          gammas <- numeric(0L)
-                          for (mm in methods) {
-                              resJ <- getJointFWERThresholds(mat, tau=mm, alpha, maxSteps=maxSteps, kMax=kMax)
-                              gammas <- c(gammas, resJ$lambda)
-                          }
-                          gammas
-                      }, mc.cores=3)
-    resRR <- do.call(cbind, resRR)
-    res[rr,,] <- resRR
-}
-
-
+saveRDS(res, file="resData/gamma.rds")
+res <- readRDS("resData/gamma.rds")
+boxplot(t(res[, 1,]), ylab=expression(gamma), xlab=expression(rho), main="gamma adjustment -- Simes thresholds")
 
