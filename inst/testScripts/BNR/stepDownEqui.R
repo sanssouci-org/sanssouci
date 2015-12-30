a2be42297e99fac7c7ae0c27fa2e9b154fdb039d
Pierre Neuvial <pierre.neuvial@math.cnrs.fr>
2015-12-30 10:20:32 +0000

    added Pareto SNR under H1

--- inst/testScripts/BNR/stepDownEqui.R
+++ inst/testScripts/BNR/stepDownEqui.R
@@ -1,7 +1,20 @@
 library("sansSouci")
+library("actuar")
 
+testStepDown <- function(m, rho, B, pi0, SNR, alpha) {
+    if (is.character(SNR)) {
+        pattern <- "Pareto\\(([0-9]),([0-9]),([0-9]))"
+        xmin <- as.numeric(gsub(pattern, "\\1", SNR))
+        shape <- as.numeric(gsub(pattern, "\\2", SNR))
+        scale <- as.numeric(gsub(pattern, "\\3", SNR))
 
-testStepDown <- function(m, rho, B, pi0, alpha) {
+        if (is.na(xmin) || is.na(shape) || is.na(scale)) {
+            stop("'SNR' parameter ill-specified in 'testStepDown'")
+        }
+        
+        m1 <- round(m*(1-pi0))
+        SNR <- xmin + rpareto(m1, shape, scale)
+    }
     sim <- simEqui(m, rho, B, pi0, SNR=SNR)
     X0 <- sim$X0
     x <- sim$x
@@ -12,10 +25,10 @@ testStepDown <- function(m, rho, B, pi0, alpha) {
     H1 <- which(sim$H==1)
     m0 <- length(H0)
     m1 <- length(H1)
-    cols <- c(1, 2)[1+H]
+#    cols <- c(1, 2)[1+H]
     
     # SD control
-    resSD <- stepDownControl(x, X0, tau="kFWER", alpha=alpha, verbose=TRUE)
+    resSD <- stepDownControl(x, X0, tau="kFWER", alpha=alpha, verbose=FALSE)
     thrMat <- resSD$thrMat
     
     # Final thresholds
@@ -45,18 +58,51 @@ testStepDown <- function(m, rho, B, pi0, alpha) {
 # Parameters:
 m <- 1e3
 rho <- 0
-pi0 <- 0.8
+pi0 <- 0.9
 B <- 5e3
-SNR <- 2
+#SNR <- 2
 alpha <- 0.25
+nbSimu <- 1e3
+
+#SNRs <- c(0, 1, 2, 3, 10)
+SNRs <- "Pareto(2,2,1)"
+rhos <- c(0, 0.2, 0.4)
+pi0s <- c(0.9, 0.99, 0.999)
+
+library("parallel")
+ncores <- 38
+
+for (SNR in SNRs) {
+    for (rho in rhos) {
+        for (pi0 in pi0s) {
+            
+
+tags <- sprintf("m=%s,pi0=%s,rho=%s,SNR=%s,B=%s,alpha=%s,nbSimu=%s",
+                m, pi0, rho, SNR, B, alpha, nbSimu)
+sname <- "stepDownEqui"
+filename <- sprintf("%s,%s.rds", sname, gsub("\\.", "_", tags))
+path <- "resData"
+path <- file.path(path, sname)
+path <- R.utils::Arguments$getWritablePath(path)
+
+print(tags)
 
 #set.seed(0xBEEF)
-library("parallel")
-nbSimu <- 8
-ncores <- 4
 res <- mclapply(1:nbSimu, FUN=function(bb) {
-    print(bb)
-    res <- testStepDown(m, rho, B, pi0, alpha)
+    if (bb %% 100 == 0) {
+        print(bb)
+    }
+    #print(system.time(res <- testStepDown(m, rho, B, pi0, alpha)))
+    res <- testStepDown(m, rho, B, pi0, SNR, alpha)
     return(res)
 }, mc.cores=ncores)
 
+pathname <- file.path(path, filename)
+saveRDS(res, file=pathname)
+
+
+#print(tags)
+resMat <- sapply(res, FUN=function(x) x[c(1, length(x)-c(1, 0))])
+rowMeans(resMat>0)
+
+}}}
