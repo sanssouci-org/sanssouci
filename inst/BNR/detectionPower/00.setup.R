4c7281e4bac13af5d05fe5dee8f95403040ebebc
Pierre Neuvial <pneuvial@gmail.com>
2017-01-31 18:21:47 +0100

    new test scripts for the BNR paper where the two notions of power are treated separately

---
+++ inst/BNR/detectionPower/00.setup.R
@@ -0,0 +1,26 @@
+library("sansSouci")
+
+typeOfSNR <- "constant"
+flavor <- "equi"
+sname0 <- sprintf("%s,%s", flavor, typeOfSNR)
+ptag <- sprintf("sansSouci_%s", packageVersion("sansSouci"))
+sname <- sprintf("%s,%s", sname0, ptag)
+
+SNRs <- c(0, 1, 2, 3, 4, 5)
+deps <- c(0, 0.2, 0.4) ## correlation coefficient
+pi0s <- c(0.8, 0.9, 0.99, 0.999)
+alphas <- c(0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.4, 0.5)
+
+m <- 1e3
+B <- 1e3
+nbSimu <- 1e3
+
+pname <- sprintf("m=%s,B=%s,nbSimu=%s", m, B, nbSimu)
+resPath <- "resData"
+path <- file.path(resPath, sname, pname)
+path <- R.utils::Arguments$getWritablePath(path)
+
+##configs <- expand.grid(alpha=alphas, pi0=pi0s, dep=deps)
+configs <- expand.grid(pi0=pi0s, dep=deps, SNR=SNRs)
+configs
+
---
+++ inst/BNR/detectionPower/01.runSimulations.R
@@ -0,0 +1,44 @@
+library("future")
+library("listenv")
+##computeNodes <- c("cauchy", "leibniz", "bolzano", "shannon", "euler", "hamming", "bernoulli")
+plan(remote, workers = rep("bernoulli", 100))
+res <- listenv()
+for (ii in 1:nrow(configs)) {
+    pi0 <- configs[ii, "pi0"]
+    dep <- configs[ii, "dep"]
+    SNR <- configs[ii, "SNR"]
+    kMaxs <- c(max(round(m*(1-pi0)), 2), m/2, m)
+    
+    tags <- sprintf("pi0=%s,dep=%s,SNR=%s", pi0, dep, SNR)
+    print(tags)
+    filename <- sprintf("%s.rds", gsub("\\.", "_", tags))
+    pathname <- file.path(path, filename)
+    if (file.exists(pathname)) {
+        next;
+    }    
+    
+    ## using futures =)
+    res <- listenv()
+    options("future.wait.times"=1e4)
+    for (ss in 1:nbSimu) {
+        if (ss %% 100==0) { print(ss);}
+        res[[ss]] %<-% {
+            library("sansSouci")
+            pathname <- system.file("BNR/testStepDown.R", package="sansSouci")
+            stopifnot(file.exists(pathname))
+            source(pathname); rm(pathname)
+            testStepDown(m, dep, B, pi0, SNR, typeOfSNR, alphas, kMaxs=kMaxs, flavor=flavor)
+        }
+    }
+
+    ## summarize into JFWER and Power estimates
+    mres <- reshape2::melt(as.list(res))
+    names(mres) <- c("risk", "flavor", "value", "family", "kMax", "alpha", "sid")
+    
+    ## summarize into JFWER and Power estimates
+    cres <- reshape2::dcast(mres, kMax+alpha+family+flavor~risk, mean, value.var="value")
+    dat <- cbind(pi0=pi0, dep=dep, SNR=SNR, cres)
+    head(dat)
+    
+    saveRDS(dat, file=pathname)
+}
