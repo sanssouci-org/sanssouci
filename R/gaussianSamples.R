38940775fb2d4388db11fccf11c7ec59c49d2294
Pierre Neuvial <pneuvial@gmail.com>
2018-03-28 23:10:28 +0200

    update doc

--- R/gaussianSamples.R
+++ R/gaussianSamples.R
@@ -1,5 +1,5 @@
 #' Simulate equi-correlated data
-#' 
+#'
 #' Simulate equi-correlated data coming from one or two populations
 #'
 #' @param m Number of hypotheses
@@ -8,18 +8,25 @@
 #' @param pi0 Proportion of true null hypotheses
 #' @param SNR Signal to noise ratio. Either a numeric value (a measure of
 #'   distance between H0 and H1) or a vector of length \code{m*(1-pi0)}
-#' @param prob A numeric value in (0, 1], the frequency of the population 
-#'   under H1. If \eqn{prob=1} (the default), a data set from a single 
-#'   population is generated.
+#' @param prob A numeric value in (0, 1], the frequency of the population under
+#'   H1. If \eqn{prob=1} (the default), a data set from a single population is
+#'   generated.
 #' @param w An optional vector of length \code{n}, the underlying factor driving
 #'   equi-correlation
-#' @return A list with two elements: \describe{
-#' \item{X}{An \eqn{m x n} matrix of \eqn{m}-dimensional Gaussian observations, where \eqn{m1} rows are under H1, and \eqn{m0} rows are under H0, with \eqn{mO/m = pi0}}
-#' \item{H}{A vector of length \eqn{m}, the status of each
-#' hypothesis: 0 for true null hypothesis, and 1 for true alternative
-#' hypothesis}}.
+#' @return A list with two elements: \describe{ \item{X}{An \eqn{m x n} matrix
+#'   of \eqn{m}-dimensional Gaussian observations, where \eqn{m1} rows are under
+#'   H1, and \eqn{m0} rows are under H0, with \eqn{m0/m = pi0}} \item{H}{A
+#'   vector of length \eqn{m}, the status of each hypothesis: 0 for true null
+#'   hypothesis, and 1 for true alternative hypothesis}}.
 #' @author Gilles Blanchard, Pierre Neuvial and Etienne Roquain
-#' @details. If \code{prob = 1}, each of m1 variables under H1 has mean \eqn{SNR/\sqrt(n)}. If \code{0 < p < 1} n0 samples are drawn from a N(0,1) distribution while n1 are drawn from a N(mu, 1) distribution, where \eqn{mu = SNR*sqrt(1/(n0) + 1/n1)}. The argument \code{p} is the probability of a sample to belong to the non-zero-mean population
+#'
+#' @details If \code{prob = 1}, each of \eqn{m_1} variables under \eqn{H_1} has
+#'   mean \eqn{SNR/\sqrt(n)}. If \code{0 < p < 1}, then \eqn{n_0} samples are
+#'   drawn from a \eqn{\mathcal{N}(0,1)} distribution while \eqn{n_1} are drawn
+#'   from a \eqn{\mathcal{N}(\mu, 1)} distribution, where \eqn{\mu =
+#'   SNR*\sqrt(1/(n_0) + 1/n_1)}. The argument \code{p} is the probability of a
+#'   sample to belong to the non-zero-mean population.
+#'
 #' @export
 #' @importFrom stats rbinom
 #' @examples
@@ -49,7 +56,7 @@
 #' colStat <- 1+sim$H
 #' plot(tests$T, col=colStat, main="Test statistics", pch=pch)
 #' legend("topleft", c("H0", "H1"), pch=pch, col=1:2)
-#'
+#' 
 gaussianSamples <- function(m, rho, n, pi0, SNR = 1, prob = 1, w = NULL) {
     m0 <- round(m*pi0)
     m1 <- m - m0
