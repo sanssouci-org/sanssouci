e44f54d1f542a80049d167a1a90ea080b7f696e1
Pierre Neuvial <pneuvial@gmail.com>
2019-10-19 01:56:19 +0200

    colorblind friendly DBNR figures [CI skip]

--- inst/DBNR/envelopes/04_ggplot-paper.R
+++ inst/DBNR/envelopes/04_ggplot-paper.R
@@ -6,7 +6,7 @@ resPath <- R.utils::Arguments$getReadablePath(resPath)
 figPath <- R.utils::Arguments$getReadablePath("fig/DBNR")
 
 # line colors
-lvls <- c("Oracle", "part", "Simes", "tree", "hybrid")
+lvls <- c("Oracle", "Partition", "Simes", "Tree", "Hybrid")
 cols <- RColorBrewer::brewer.pal(length(lvls), "Set1")
 names(cols) <- lvls
 lblr <- label_bquote(
@@ -25,10 +25,13 @@ pathname <- file.path(resPath, filename)
 dat <- readRDS(pathname)
 rm(pathname)
 dim(dat)
-        
 library("magrittr")
 library("dplyr")
 library("ggplot2")
+
+dat$method <- recode_factor(dat$method, 
+                            "tree" = "Tree",
+                            "part" = "Partition")
 
 xymax <- 800
 ymax <- 601
@@ -57,20 +60,21 @@ for (pp in 1:nrow(plotConfigs)) {
     sdat <- subset(gdat, alpha == aa & order == oo & setting == ss)
     sdat$method <- factor(sdat$method, levels = lvls)
     
-    ggplot(sdat, aes(idxs, idxs - value, colour = method)) + 
+    ggplot(sdat, aes(idxs, idxs - value, color = method)) + 
+        geom_abline(slope = 1, intercept = 0, col = "lightgray", lty = 2) +
         geom_line() + 
+        geom_point(aes(shape = method)) +
         facet_grid(r ~ barmu, labeller = lblr) +
         # xlim(1, xymax) + ylim(0, xymax) +   
         # ylab(sprintf("%s%% upper confidence envelope on the number of false positives", (1-aa)*100)) +
         xlim(1, xymax) + ylim(0, ymax) +   
         ylab(sprintf("%s%% lower confidence envelope on the number of true positives", (1-aa)*100)) +
         xlab(sprintf("Hypotheses sorted by %s", oo)) +
-        # geom_abline(slope = 1, intercept = 0, col = "lightgray", lty = 2) +
         scale_linetype(name = expression(alpha)) +
-        scale_colour_manual(values = cols)
+        scale_colour_manual(values = cols) 
+    
     ggsave(filename = pathname, width = pwidth, height = pheight)
 }
-
 
 ## 2- influence of alpha -- all in one
 ## now focus on order = p and grouped
@@ -95,15 +99,23 @@ for (pp in 1:nrow(plotConfigs)) {
     pathname <- file.path(figPath, ff)
     
     sdat <- subset(gdat, setting == ss & order == oo & barmu > 2 & r < 1)
+    #    sdat <- subset(gdat, setting == ss & order == oo & barmu == 2 & r == 0.9)
     sdat$alpha <- as.factor(sdat$alpha)
-    ggplot(sdat, aes(idxs, idxs - value, colour = method, linetype = alpha)) + 
-        geom_line() + facet_grid(r ~ barmu, labeller = lblr) +
+    ggplot(sdat, aes(idxs, idxs - value, 
+                     colour = method, 
+                     linetype = alpha)) + 
+        geom_abline(slope = 1, intercept = 0, col = "lightgray", lty = 2) +
+        geom_line() + 
+        geom_point(aes(shape = method)) +
+        facet_grid(r ~ barmu, labeller = lblr) +
+        # facet_wrap(vars(method)) +
+        # geom_point(aes(shape = method)) +
+        
         # xlim(1, xymax) + ylim(0, xymax) +
         # ylab("Upper confidence envelope on the number of false positives") +
         xlim(1, xymax) + ylim(0, ymax) +   
         ylab("Lower confidence envelope on the number of true positives") +
         xlab(sprintf("Hypotheses sorted by %s", oo)) +
-        # geom_abline(slope = 1, intercept = 0, col = "lightgray", lty = 2) +
         scale_linetype(name = expression(alpha)) +
         scale_colour_manual(values = cols)
     ggsave(filename = pathname, width = pwidth, height = pheight)
@@ -119,10 +131,10 @@ gdat <- filter(dat, s == 100 & K1 == 8
     rename(r = d) %>%
     arrange(idxs, method, r, barmu, alpha, order)
 simes <- filter(gdat, method == "Simes" & alpha == 0.049) 
-tree <- filter(gdat, method == "tree" & alpha == 0.001)
+tree <- filter(gdat, method == "Tree" & alpha == 0.001)
 bound <- pmin(select(simes, value), select(tree, value))
 hyb <- simes %>% select(-value, -alpha, -method) %>%
-    mutate(method = "hybrid", 
+    mutate(method = "Hybrid", 
            alpha = 0.05, 
            value = bound$value)
 gdat <- gdat %>% mutate(method = as.character(method))%>%
@@ -143,11 +155,11 @@ for (pp in 1:nrow(plotConfigs)) {
     ftag <- gsub("\\.", "-", ftag)
     ff <- sprintf("%s_%s.pdf", figName, ftag)
     pathname <- file.path(figPath, ff)
-
+    
     sdat <- subset(gdat, order == oo & barmu >= 2 & r <= 1 & method != "part" & setting == ss)
     ssdat <- subset(sdat,  method == "Simes" & alpha %in% c(0.001, 0.05))
-    tsdat <- subset(sdat,  method == "tree" & alpha %in% c(0.001, 0.05))
-    hsdat <- subset(sdat,  method == "hybrid" & alpha == 0.05)
+    tsdat <- subset(sdat,  method == "Tree" & alpha %in% c(0.001, 0.05))
+    hsdat <- subset(sdat,  method == "Hybrid" & alpha == 0.05)
     osdat <- subset(sdat,  method == "Oracle" & alpha == 0.05)
     sdat <- rbind(ssdat, tsdat, hsdat, osdat)
     sdat$alpha <- as.factor(sdat$alpha)
@@ -155,7 +167,9 @@ for (pp in 1:nrow(plotConfigs)) {
     sdat$method <- factor(sdat$method, levels = lvls)
     
     ggplot(sdat, aes(idxs, idxs - value, colour = method, linetype = alpha)) + 
+        geom_abline(slope = 1, intercept = 0, col = "lightgray", lty = 2) +
         geom_line() + 
+        geom_point(aes(shape = method)) +
         facet_grid(r ~ barmu, 
                    labeller = label_bquote(
                        rows = r == .(r), 
@@ -165,7 +179,6 @@ for (pp in 1:nrow(plotConfigs)) {
         xlim(1, xymax) + ylim(0, ymax) +
         ylab("Lower confidence envelope on the number of true positives") +
         xlab(sprintf("Hypotheses sorted by %s", oo)) +
-        geom_abline(slope = 1, intercept = 0, col = "lightgray", lty = 2) +
         scale_linetype(name=expression(alpha)) +
         scale_colour_manual(values = cols)
     ggsave(filename = pathname, width = pwidth, height = pheight)
