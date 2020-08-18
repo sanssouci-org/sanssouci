4f60074dbd152c748995d45cd91df674fc0ae3d7
Pierre Neuvial <pneuvial@gmail.com>
2020-08-18 23:41:35 +0200

    cleanups in the package data

--- inst/system/create-volcano-plot-data.R
+++ inst/system/create-volcano-plot-data.R
@@ -1,23 +1,25 @@
-dataSets <- c("bourgon", "golub", "hedenfalk", "loi", "rosenwald")
+dataSets <- c("chiaretti", "golub", "hedenfalk", "loi", "rosenwald")
 dataSets <- dataSets[-4] ## Dropping Loi: too weak signal
 
 getStats <- function(dataSet) {
     filename <- sprintf("%s.rds", dataSet)
-    pathname <- file.path("data", filename)
+    pathname <- file.path("local/data", filename)
     dat <- readRDS(pathname)
     rm(pathname)
     
-    res <- rowWelchTests(dat, colnames(dat))
+    cn <- colnames(dat)[1]
+    categ <- ifelse(colnames(dat) == cn, 1, 0) # map to 0/1
+    res <- rowWelchTests(dat, categ)
     mres <- Reduce(cbind, res)
     colnames(mres) <- names(res)
     
     m <- nrow(mres)
     gid <- rownames(mres)
     if (is.null(gid)) {
-        gid <- paste("gene", 1:m, sep = "-")
+        patt <- sprintf("gene_%%0%ds", floor(log10(m)) + 1)
+        gid <- sprintf(patt, 1:m)
     }
     p <- mres[ ,"p.value"]
-    logp <- -log10(p)
     adjp <- p.adjust(p, method = "BH")
     dat <- data.frame(id = gid, 
                  dataSet = dataSet, 
@@ -31,5 +33,6 @@ getStats <- function(dataSet) {
 
 dfList <- lapply(dataSets, getStats)
 volcano <- Reduce(rbind, dfList)
-save(volcano, file = "data/volcano.rda")
-tools::resaveRdaFiles("data/volcano.rda")  ## why isn't this the default in 'save'?
+save(volcano, file = "data/volcano_plot.rda", compress = "xz")
+#tools::checkRdaFiles("data/volcano_plot.rda")
+#tools::resaveRdaFiles("data/volcano_plot.rda")  ## why isn't this the default in 'save'?
