35883926d1ba1a16fc98ff1a4b51cf4564dcd7e0
Pierre Neuvial <pierre.neuvial@math.cnrs.fr>
2021-03-11 23:20:55 +0100

    test that 'bound.SansSouci' reproduces the results of 'curveMaxFP'

--- tests/testthat/test-SansSouci-class.R
+++ tests/testthat/test-SansSouci-class.R
@@ -1,13 +1,15 @@
 test_that("Correctness of the constructor of SansSouci", {
     m <- 54
-    n <- 13
+    n <- 132
     set.seed(0xBEEF)
     obj <- SansSouciSim(m = m, rho = 0, n = n, 
                              pi0 = 0.8, SNR = 0, prob = 0.4)
+    expect_s3_class(obj, "SansSouci")
     set.seed(0xBEEF)
     sim <- gaussianSamples(m = m, rho = 0, n = n, 
                            pi0 = 0.8, SNR = 0, prob = 0.4)
     obj1 <- SansSouci(Y = sim$X, groups = sim$categ, truth = sim$H)
+    expect_s3_class(obj1, "SansSouci")
     expect_identical(obj1, obj)
     
     expect_equal(names(obj), c("input", "parameters", "output"))
@@ -31,7 +33,7 @@ test_that("Correctness of the constructor of SansSouci", {
 
 test_that("Correctness of elements of fitted  'SansSouci' object", {
     m <- 54
-    n <- 13
+    n <- 132
     
     alpha <- 0.1
     B <- 25
@@ -41,6 +43,7 @@ test_that("Correctness of elements of fitted  'SansSouci' object", {
     alt <- "greater"
     fam <- "Beta"
     res <- fit(obj, alpha = alpha, B = B, K = K, alternative = alt, family = fam)
+    expect_s3_class(res, "SansSouci")
     expect_identical(names(res), names(obj))
     params <- res$parameters
     names(params)
@@ -55,7 +58,7 @@ test_that("Correctness of elements of fitted  'SansSouci' object", {
 
 test_that("'fit.SansSouci' reproduces the results of 'calibrateJER'", {
     m <- 54
-    n <- 13
+    n <- 132
     obj <- SansSouciSim(m = m, rho = 0, n = n, 
                          pi0 = 0.8, SNR = 0, prob = 0.4)
     Y <- obj$input$Y
@@ -80,3 +83,65 @@ test_that("'fit.SansSouci' reproduces the results of 'calibrateJER'", {
     }
 })
 
+test_that("Consistency of output of 'bound.SansSouci'", {
+    m <- 54
+    n <- 132
+    
+    alpha <- 0.1
+    B <- 25
+    K <- m/2
+    obj <- SansSouciSim(m = m, rho = 0, n = n, 
+                        pi0 = 0.8, SNR = 3, prob = 0.4)
+    alt <- "greater"
+    fam <- "Beta"
+    res <- fit(obj, alpha = alpha, B = B, K = K, alternative = alt, family = fam)
+    res <- fit(obj, alpha = alpha, B = B)
+    what0 <- c("FP", "TP", "FDP", "TDP")
+    
+    # 'all=FALSE' => return a vector
+    b <- bound(res, what = what0)
+    expect_type(b, "double")
+    expect_identical(names(b), what0)
+    expect_equal(b[["FDP"]] + b[["TDP"]], 1)
+    expect_equal(b[["FP"]] + b[["TP"]], m)
+    
+    # 'all=FALSE' => return a data.frame
+    b <- bound(res, what = what0, all = TRUE)
+    expect_s3_class(b, "data.frame")
+    expect_equal(nrow(b), m * length(what0))
+    
+    # strict subset
+    S <- order(pValues(res))[seq_len(m-10)]
+    bb <- bound(res, S, what = what0, all = TRUE)
+    expect_s3_class(bb, "data.frame")
+    expect_equal(nrow(bb), length(S) * length(what0))
+    names(bb)
+    
+    expect_equivalent(subset(b, x <= length(S)), bb)
+    
+    ww <- which(bb$x == length(S))
+    w <- which(b$x == length(S))
+    expect_equivalent(b[w, ], bb[ww, ])
+})    
+
+
+test_that("'bound.SansSouci' reproduces the results of 'curveMaxFP'", {
+    m <- 54
+    n <- 132
+    
+    alpha <- 0.1
+    B <- 25
+    K <- m/2
+    obj <- SansSouciSim(m = m, rho = 0, n = n, 
+                        pi0 = 0.8, SNR = 3, prob = 0.4)
+    alt <- "greater"
+    fam <- "Beta"
+    res <- fit(obj, alpha = alpha, B = B, K = K, alternative = alt, family = fam)
+    what0 <- c("FP", "TP", "FDP", "TDP")
+    
+    pvals <- sort(pValues(res))
+    FP <- sansSouci:::curveMaxFP(p.values = pvals, 
+                                 thr = thresholds(res))
+    FPb <- bound(res, what = "FP", all = TRUE)$bound
+    expect_identical(FPb, FP)
+})
