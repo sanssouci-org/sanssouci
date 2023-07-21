8232f08ef7e5c2678420a03659bbad59052854b0
Guillermo Durand <guillermo.durand.2011@polytechnique.org>
2023-07-21 11:14:04 +0200

    liniting

--- inst/DBNR/envelopes-hybrid/script_refining_benefit.R
+++ inst/DBNR/envelopes-hybrid/script_refining_benefit.R
@@ -8,10 +8,10 @@ leaf_list <- example$leaf_list
 C <- example$C
 
 pval_idx <- unlist(leaf_list)
-pval <- runif(n=max(pval_idx))
+pval <- runif(n = max(pval_idx))
 
-alpha=0.05
-method<-zeta.HB
+alpha = 0.05
+method <- zeta.HB
 
 pval[1:4]<-1e-15
 
@@ -19,10 +19,10 @@ print("The pvalues are:")
 print(pval)
 
 print("Without refining:")
-zetas_no <- zetas.tree.no.extension(C, leaf_list, method, pval, alpha, refine=FALSE, verbose=TRUE)
+zetas_no <- zetas.tree.no.extension(C, leaf_list, method, pval, alpha, refine = FALSE, verbose = TRUE)
 print("The zetas are:")
 print(zetas_no)
 print("With refining:")
-zetas_yes <- zetas.tree.no.extension(C, leaf_list, method, pval, alpha, refine=TRUE, verbose=TRUE)
+zetas_yes <- zetas.tree.no.extension(C, leaf_list, method, pval, alpha, refine = TRUE, verbose = TRUE)
 print("The zetas are:")
 print(zetas_yes)
--- inst/DBNR/envelopes-hybrid/script_test_pruning.R
+++ inst/DBNR/envelopes-hybrid/script_test_pruning.R
@@ -9,54 +9,54 @@ leaf_list <- example$leaf_list
 C <- example$C
 
 pval_idx <- unlist(leaf_list)
-pval <- runif(n=max(pval_idx))
+pval <- runif(n = max(pval_idx))
 
-alpha=0.05
-method<-zeta.HB
+alpha = 0.05
+method <- zeta.HB
 
-pval[1:4]<-1e-15
+pval[1:4] <- 1e-15
 
 print("The pvalues are:")
 print(pval)
 
-zetas <- zetas.tree.no.extension(C, leaf_list, method, pval, alpha, refine=TRUE, verbose=FALSE)
+zetas <- zetas.tree.no.extension(C, leaf_list, method, pval, alpha, refine = TRUE, verbose = FALSE)
 print("The zetas are:")
 print(zetas)
 
 pruned <- pruning(C, zetas, leaf_list)
-print(paste0("V^*({1, ..., ",m,"})=",pruned$VstarNm))
+print(paste0("V^*({1, ..., ", m, "})=", pruned$VstarNm))
 print("New structure:")
 print(pruned$C)
 print("New associated zetas:")
 print(pruned$ZL)
 
 print("Testing the pruned structure")
-print(paste0("V^*({1, ..., ",m,"})=",V.star.no.extension(1:m, pruned$C, pruned$ZL, leaf_list)))
-print(paste0("V^*({1, ..., ",5,"})=",V.star.no.extension(1:5, pruned$C, pruned$ZL, leaf_list)))
-print(paste0("V^*({1, ..., ",8,"})=",V.star.no.extension(1:8, pruned$C, pruned$ZL, leaf_list)))
-print(paste0("V^*({9, ..., ",16,"})=",V.star.no.extension(9:16, pruned$C, pruned$ZL, leaf_list)))
+print(paste0("V^*({1, ..., ", m, "})=", V.star.no.extension(1:m, pruned$C, pruned$ZL, leaf_list)))
+print(paste0("V^*({1, ..., ", 5, "})=", V.star.no.extension(1:5, pruned$C, pruned$ZL, leaf_list)))
+print(paste0("V^*({1, ..., ", 8, "})=", V.star.no.extension(1:8, pruned$C, pruned$ZL, leaf_list)))
+print(paste0("V^*({9, ..., ", 16, "})=", V.star.no.extension(9:16, pruned$C, pruned$ZL, leaf_list)))
 
 
 print("Changing for an arbitrary zeta tree to run more tests")
 arbitrary_zetas <- list(10,
 							c(4, 6),
-							c(0,4,3,4)
+							c(0, 4, 3 , 4)
 							)
 print("The zetas now are:")
 print(arbitrary_zetas)
 
 pruned2 <- pruning(C, arbitrary_zetas, leaf_list)
-print(paste0("V^*({1, ..., ",m,"})=",pruned2$VstarNm))
+print(paste0("V^*({1, ..., ", m, "})=", pruned2$VstarNm))
 print("New structure:")
 print(pruned2$C)
 print("New associated zetas:")
 print(pruned2$ZL)
 
 print("Testing the pruned structure")
-print(paste0("V^*({1, ..., ",m,"})=",V.star.no.extension(1:m, pruned2$C, pruned2$ZL, leaf_list)))
-print(paste0("V^*({1, ..., ",5,"})=",V.star.no.extension(1:5, pruned2$C, pruned2$ZL, leaf_list)))
-print(paste0("V^*({1, ..., ",8,"})=",V.star.no.extension(1:8, pruned2$C, pruned2$ZL, leaf_list)))
-print(paste0("V^*({9, ..., ",16,"})=",V.star.no.extension(9:16, pruned2$C, pruned2$ZL, leaf_list)))
+print(paste0("V^*({1, ..., ", m, "})=", V.star.no.extension(1:m, pruned2$C, pruned2$ZL, leaf_list)))
+print(paste0("V^*({1, ..., ", 5, "})=", V.star.no.extension(1:5, pruned2$C, pruned2$ZL, leaf_list)))
+print(paste0("V^*({1, ..., ", 8, "})=", V.star.no.extension(1:8, pruned2$C, pruned2$ZL, leaf_list)))
+print(paste0("V^*({9, ..., ", 16, "})=", V.star.no.extension(9:16, pruned2$C, pruned2$ZL, leaf_list)))
 
 print("Now we test the computation time enhancement provided by the pruning, on S={1, ..., m} and S={5}")
 mbench <- microbenchmark(no_pruning = V.star.no.extension(1:m, C, zetas, leaf_list), 
