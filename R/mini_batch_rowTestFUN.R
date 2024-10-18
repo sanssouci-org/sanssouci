7ae94e411743890abd2a1b23ccd327811e3fd46f
enjalbert-courrech nicolas <nenjalbe@audriza.math.univ-toulouse.fr>
2024-10-18 17:26:26 +0200

    fix documentation issues

--- R/mini_batch_rowTestFUN.R
+++ R/mini_batch_rowTestFUN.R
@@ -1,13 +1,19 @@
 #' Mini batch version of a rowTestFUN function.
 #'
-#' @param rowTestFUN a function taking as inputs Y, categ and Alternative and 
+#' @param rowTestFUN A function taking as inputs Y, categ and Alternative and 
 #' giving as a result a matrix of $p$-values. Example \code{\link{rowWelchTests}} or 
 #' \code{\link{rowWilcoxonTests}}.
-#' @param Y a $m \times n$ numeric matrix whose rows correspond to variables
+#' @param Y A \eqn{m \times n} numeric matrix whose rows correspond to variables
 #' and columns to observations
-#' @param categ 
-#' @param alternative 
-#' @param max_batch_size 
+#' @param categ Either a numeric vector of \code{n} categories in \eqn{0, 1} for
+#'   the observations, or a \code{n x B} matrix stacking \code{B} such vectors
+#'   (typically permutations of an original vector of size \code{n})
+#' @param alternative A character string specifying the alternative hypothesis.
+#'   Must be one of "two.sided" (default), "greater" or "less". As in
+#'   \code{\link{t.test}}, alternative = "greater" is the alternative that class
+#'   1 has a larger mean than class 0.
+#' @param max_batch_size A number specifying the maximum size of information in 
+#'   each batch
 #'
 #' @return the p-values for the test
 #' @export
@@ -18,7 +24,7 @@
 #' mat <- matrix(rnorm(p * n), ncol = n)
 #' cls <- rep(c(0, 1), times = c(10, n - 10))
 #' mini_batch_rowTestFUN(rowTestFUN = rowWelchTests, Y = mat, 
-#'                       categ = cls, alternative = alt, 
+#'                       categ = cls, alternative = "two.sided", 
 #'                       max_batch_size = 1e6)
 mini_batch_rowTestFUN <- function(rowTestFUN, Y, categ,
                                   alternative = c("two.sided", "less", "greater"), 
