7dfe6e670c0442c02778a6b21dad3273df3954e1
Guillermo Durand <guillermo.durand@universite-paris-saclay.fr>
2025-04-11 17:27:20 +0200

    start working on new fast implementation

---
+++ inst/DBNR/envelopes-hybrid/new_impl_fast.R
@@ -0,0 +1,257 @@
+curve.V.star.forest.fast.14hypcol <- function(perm, C, ZL, leaf_list, pruning = FALSE, is.pruned = FALSE, is.complete = FALSE, delete.gaps = FALSE){
+  
+  vstars <- numeric(length(perm))
+  
+  if (! is.pruned) {
+    
+    if (! is.complete) {
+      # the fast version needs a proper completion of the
+      # forest structure, and for the same reason
+      # it must not prune the leaves
+      completed <- forest.completion(C, ZL, leaf_list)
+      C <- completed$C
+      ZL <- completed$ZL
+    }
+    
+    if (pruning) {
+      is.pruned <- TRUE # useless atm, but kept for clarity
+      pruned <- pruning(C, ZL, leaf_list, prune.leafs = FALSE, delete.gaps = delete.gaps)
+      C <- pruned$C
+      ZL <- pruned$ZL
+      m <- length(unlist(leaf_list))
+      
+      if (length(perm) == m) {
+        # means that the last bound to compute
+        # is V^*({1, ..., m}),
+        # but the pruning already computed
+        # V^*({1, ..., m}) as a by-product so we
+        # might as well use it:
+        vstars[m] <- pruned$VstarNm
+        perm <- perm[-m]
+      }
+    }
+  }
+  
+  H <- length(C)
+  
+  etas <- ZL
+  K.minus <- list()
+  for (h in 1:H){
+    etas[[h]] <- rep(0, length(ZL[[h]]))
+    K.minus[[h]] <- list()
+    if (length(ZL[[h]]) > 0){
+      for (j in 1:length(ZL[[h]])){
+        if (ZL[[h]][j] == 0){
+          K.minus[[h]][[j]] <- C[[h]][[j]]
+        }
+      }
+    }
+  }
+  
+  for (t in 1:length(perm)) {
+    
+    i.t <- perm[t]
+    if (t > 1) {
+      previous.vstar <- vstars[t - 1]
+    } else {
+      previous.vstar <- 0
+    }
+    
+    ################################
+    # SEARCHING IF i_t IS IN K MINUS
+    # if so, go.next == TRUE
+    # and we just go next to step t+1
+    go.next <- FALSE
+    for (h in 1:H) {
+      if (go.next) {
+        break
+      }
+      for (couple in K.minus[[h]]) {
+        if (! is.null(couple)) {
+          lower_leaf <- leaf_list[[couple[1]]]
+          lower_hyp <- lower_leaf[1]
+          upper_leaf <- leaf_list[[couple[2]]]
+          upper_hyp <- upper_leaf[length(upper_leaf)]
+          if ((i.t >= lower_hyp) && (i.t <= upper_hyp)) {
+            go.next <- TRUE
+            # print(paste0(i.t, " is in K minus"))
+            break
+          }
+        }
+      }
+    }
+    # print(paste0(i.t, " isn't in K minus"))
+    #########################################
+    
+    # COMPUTING V.STAR AND UPDATING K.MINUS AND ETAS
+    ################################################
+    if (go.next) {
+      vstars[t] <- previous.vstar
+    } else {
+      # Here, i_t isn't in K minus
+      for (h in 1:H) {
+        nb_regions <- length(C[[h]])
+        if(nb_regions > 0){
+          is.found <- FALSE
+          for (j in 1:nb_regions) {
+            couple <- C[[h]][[j]]
+            lower_leaf <- leaf_list[[couple[1]]]
+            lower_hyp <- lower_leaf[1]
+            upper_leaf <- leaf_list[[couple[2]]]
+            upper_hyp <- upper_leaf[length(upper_leaf)]
+            if((i.t >= lower_hyp) && (i.t <= upper_hyp)){
+              # we found k^{(t,h)}
+              is.found <- TRUE
+              break
+            }
+          }
+          if (! is.found) {
+            # there is no k^{(t,h)} because there is a 
+            # gap in the structure (because of pruning)
+            next
+          }
+          etas[[h]][[j]] <- etas[[h]][[j]] + 1
+          if(etas[[h]][[j]] < ZL[[h]][[j]]){
+            # pass
+          } else {
+            K.minus[[h]][[j]] <- C[[h]][[j]]
+            break
+          }
+        }
+      }
+      vstars[t] <- previous.vstar + 1
+    }
+    ################################################
+    
+  }
+  return(vstars)
+}
+
+curve.V.star.forest.fast.14hyprow <- function(perm, C, ZL, leaf_list, pruning = FALSE, is.pruned = FALSE, is.complete = FALSE, delete.gaps = FALSE){
+  
+  vstars <- numeric(length(perm))
+  
+  if (! is.pruned) {
+    
+    if (! is.complete) {
+      # the fast version needs a proper completion of the
+      # forest structure, and for the same reason
+      # it must not prune the leaves
+      completed <- forest.completion(C, ZL, leaf_list)
+      C <- completed$C
+      ZL <- completed$ZL
+    }
+    
+    if (pruning) {
+      is.pruned <- TRUE # useless atm, but kept for clarity
+      pruned <- pruning(C, ZL, leaf_list, prune.leafs = FALSE, delete.gaps = delete.gaps)
+      C <- pruned$C
+      ZL <- pruned$ZL
+      m <- length(unlist(leaf_list))
+      
+      if (length(perm) == m) {
+        # means that the last bound to compute
+        # is V^*({1, ..., m}),
+        # but the pruning already computed
+        # V^*({1, ..., m}) as a by-product so we
+        # might as well use it:
+        vstars[m] <- pruned$VstarNm
+        perm <- perm[-m]
+      }
+    }
+  }
+  
+  H <- length(C)
+  
+  etas <- ZL
+  K.minus <- list()
+  for (h in 1:H){
+    etas[[h]] <- rep(0, length(ZL[[h]]))
+    K.minus[[h]] <- list()
+    if (length(ZL[[h]]) > 0){
+      for (j in 1:length(ZL[[h]])){
+        if (ZL[[h]][j] == 0){
+          K.minus[[h]][[j]] <- C[[h]][[j]]
+        }
+      }
+    }
+  }
+  
+  for (t in 1:length(perm)) {
+    
+    i.t <- perm[t]
+    if (t > 1) {
+      previous.vstar <- vstars[t - 1]
+    } else {
+      previous.vstar <- 0
+    }
+    
+    ################################
+    # SEARCHING IF i_t IS IN K MINUS
+    # if so, go.next == TRUE
+    # and we just go next to step t+1
+    go.next <- FALSE
+    for (h in 1:H) {
+      if (go.next) {
+        break
+      }
+      for (couple in K.minus[[h]]) {
+        if (! is.null(couple)) {
+          lower_leaf <- leaf_list[[couple[1]]]
+          lower_hyp <- lower_leaf[1]
+          upper_leaf <- leaf_list[[couple[2]]]
+          upper_hyp <- upper_leaf[length(upper_leaf)]
+          if ((i.t >= lower_hyp) && (i.t <= upper_hyp)) {
+            go.next <- TRUE
+            # print(paste0(i.t, " is in K minus"))
+            break
+          }
+        }
+      }
+    }
+    # print(paste0(i.t, " isn't in K minus"))
+    #########################################
+    
+    # COMPUTING V.STAR AND UPDATING K.MINUS AND ETAS
+    ################################################
+    if (go.next) {
+      vstars[t] <- previous.vstar
+    } else {
+      # Here, i_t isn't in K minus
+      for (h in 1:H) {
+        nb_regions <- length(C[[h]])
+        if(nb_regions > 0){
+          is.found <- FALSE
+          for (j in 1:nb_regions) {
+            couple <- C[[h]][[j]]
+            lower_leaf <- leaf_list[[couple[1]]]
+            lower_hyp <- lower_leaf[1]
+            upper_leaf <- leaf_list[[couple[2]]]
+            upper_hyp <- upper_leaf[length(upper_leaf)]
+            if((i.t >= lower_hyp) && (i.t <= upper_hyp)){
+              # we found k^{(t,h)}
+              is.found <- TRUE
+              break
+            }
+          }
+          if (! is.found) {
+            # there is no k^{(t,h)} because there is a 
+            # gap in the structure (because of pruning)
+            next
+          }
+          etas[[h]][[j]] <- etas[[h]][[j]] + 1
+          if(etas[[h]][[j]] < ZL[[h]][[j]]){
+            # pass
+          } else {
+            K.minus[[h]][[j]] <- C[[h]][[j]]
+            break
+          }
+        }
+      }
+      vstars[t] <- previous.vstar + 1
+    }
+    ################################################
+    
+  }
+  return(vstars)
+}
---
+++ inst/DBNR/envelopes-hybrid/script_test_curve_fast_paper.R
@@ -0,0 +1,59 @@
+#! /usr/bin/env Rscript
+library(sanssouci)
+library(microbenchmark)
+
+source("inst/DBNR/envelopes-hybrid/new_impl_fast.R")
+
+set.seed(12)
+
+get_groups <- function(list_groups, leaf_list){
+  res <- list()
+  for (group in list_groups){
+    res <- c(res, list(leaf_list[[group[1]]][1]:rev(leaf_list[[group[2]]])[1]))
+  }
+  return(list(res = res, total = unlist(res)))
+}
+
+pow <- 10
+alpha <- 0.05
+
+vec_factor <- c(1, 10)
+vec_n_repl <- c(100, 10)
+vec_method <- c(zeta.DKWM, zeta.trivial)
+
+for (i in 1:4){
+  
+  factor <- vec_factor[floor((i+1)/2)]
+  n_repl <- vec_n_repl[floor((i+1)/2)]
+  method <- vec_method[[i %% 2 + 1]]
+  
+  m <- (2 ^ pow) * factor
+  example <- dyadic.from.height(m, pow, 2)
+  leaf_list <- example$leaf_list
+  C <- example$C
+  signal <- 4
+  mu <- rep(0, m)
+  H1 <- get_groups(example$C[[6]][c(1, 5, 9, 10)], leaf_list)$total
+  mu[H1] <- signal
+  
+  pval <- 1 - pnorm(mu + rnorm(n = m))
+  
+  ZL <- zetas.tree(C, leaf_list, method, pval, alpha, refine = TRUE, verbose = FALSE)
+  
+  pruned.no.gaps <- pruning(C, ZL, leaf_list, prune.leafs = FALSE, delete.gaps = TRUE)
+  
+  print(m)
+  print(nb.elements(C))
+  print(nb.elements(pruned.no.gaps$C))
+  
+  perm <- 1:m
+  
+  print("Comparing execution times:")
+  mbench <- microbenchmark(#naive.not.pruned = curve.V.star.forest.naive(perm, C, ZL, leaf_list),
+                           #naive.pruned = curve.V.star.forest.naive(perm, pruned.no.gaps$C, pruned.no.gaps$ZL, leaf_list),
+                           fast13.not.pruned = curve.V.star.forest.fast(perm, C, ZL, leaf_list),
+                           fast13.pruned = curve.V.star.forest.fast(perm, pruned.no.gaps$C, pruned.no.gaps$ZL, leaf_list, is.pruned = TRUE),
+                           times = n_repl, check = "equal")
+  print(mbench)
+  #write.csv(mbench, paste0("benchmark_0", i, ".csv"), row.names = F)
+}
