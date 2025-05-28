cbeab047b7af8a6e7c2fe7bd987c875954697054
Pierre Neuvial <pierre.neuvial@math.cnrs.fr>
2025-05-29 01:00:50 +0200

    update doc

--- R/partialColSort.R
+++ R/partialColSort.R
@@ -5,11 +5,12 @@
 #' @param mat a \code{m} x \code{B} numeric matrix
 #' @param k an integer value between 1 and \code{m}
 #' @author Gilles Blanchard, Pierre Neuvial and Etienne Roquain
-#' @return a \code{k} x \code{B} numeric matrix, whose \code{b}-th
+#' @return a \codée{k} x \code{B} numeric matrix, whose \code{b}-th
 #' column corresponds to the \code{k} largest values of the
 #' \code{b}-th column of \code{mat}, sorted decreasingly.
+#' @noRd
 #' @examples
-#'
+#' 
 #' A <- matrix(rnorm(15), 5, 3)
 #' print(A)
 #' partialColSortDesc(A, 2)
@@ -17,31 +18,28 @@
 #' 
 partialColSort <- function(
     mat,
-    k=nrow(mat),
-    Rcpp=TRUE)   {
+    k = nrow(mat)) {
     k <- round(k)  ## make sure 'k' is not converted to the wrong integer as in 'as.integer(1000*(1-0.8))==199'
     stopifnot( 1 <= k && k <= nrow(mat))
-    pcsd <- apply(mat, 2, sort, partial=1:k)
-    pcsd <- pcsd[1:k, , drop=FALSE]
+    pcsd <- apply(mat, 2, sort, partial = 1:k)
+    pcsd <- pcsd[1:k, , drop = FALSE]
 
     ## sanity checks
-    stopifnot(nrow(pcsd)==k)
-    stopifnot(ncol(pcsd)==ncol(mat))
+    stopifnot(nrow(pcsd) == k)
+    stopifnot(ncol(pcsd) == ncol(mat))
     return(pcsd)
 }
 
-#'@rdname partialColSort
 partialColSortDesc <- function(
     mat,
-    k=nrow(mat),
-    Rcpp=TRUE)   {
+    k = nrow(mat)) {
   k <- round(k)  ## make sure 'k' is not converted to the wrong integer as in 'as.integer(1000*(1-0.8))==199'
   stopifnot( 1 <= k && k <= nrow(mat))
-  pcsd <- -apply(-mat, 2, sort, partial=1:k)
-  pcsd <- pcsd[1:k, , drop=FALSE]
+  pcsd <- -apply(-mat, 2, sort, partial = 1:k)
+  pcsd <- pcsd[1:k, , drop = FALSE]
   
   ## sanity checks
-  stopifnot(nrow(pcsd)==k)
-  stopifnot(ncol(pcsd)==ncol(mat))
+  stopifnot(nrow(pcsd) == k)
+  stopifnot(ncol(pcsd) == ncol(mat))
   return(pcsd)
 }
