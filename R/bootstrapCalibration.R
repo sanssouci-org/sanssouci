694cf969fb78388df8c1166e786a57893901281a
enjalbert-courrech nicolas <nenjalbe@audriza.math.univ-toulouse.fr>
2025-05-28 11:43:04 +0200

    Add bootstrap calibration functions and unit tests

---
+++ R/bootstrapCalibration.R
@@ -0,0 +1,206 @@
+#' Perform statistical test from contrast matrix for Linear model.
+#'
+#' @param Y Expression matrix of size $n$ observations (in row) and $D$
+#' dimensions/mesures in columns
+#' @param X Design matrix of size $n$ observations (in row) and $p$ variables
+#' (in columns)
+#' @param C Contrast matrix of size $L$ tested contrast (in row) and $p$ columns
+#' corresponding to te tested parameters.
+#'
+#' @return epsilon_est, the residuals matrix (size \eqn{n \times D}), stat_test the
+#' test statistic matrix (size \eqn{L \times D}) and p-values, the p-values matrix
+#' (size \eqn{L \times D})
+#' @export
+#'
+#' @examples
+#' N = 100
+#' p = 2
+#' D = 2
+#' X <- matrix(0,nrow = N, ncol = p)
+#' X[,1] <- 1
+#' X[,-1] <- runif(N*(p-1), min = 0, max = 3)
+#' beta <- matrix(0, nrow = p, ncol = D)
+#' epsilons <- matrix(rnorm(N*D), nrow = N, ncol = D)
+#' Y <- X %*% beta + epsilons
+#' C <- diag(p)
+#' resLM <- lm_test(Y = Y, X = X, C = C)
+lm_test <- function(Y, X, C) {
+  .check_lm_test(Y, X, C)
+
+  df <- nrow(Y) - qr(X)$rank
+
+  ## model parameters estimation
+  XtX <- crossprod(X) # t(X) %*% X
+  XtY <- crossprod(X, Y) # t(X) %*% Y
+  XtX_inv <- solve(XtX)
+  beta_est <- crossprod(XtX_inv, XtY)
+
+  ## residuals
+  epsilon_est <- Y - X %*% beta_est
+
+  ## estimation of residual standard deviation
+  sigma_est <- sqrt(colSums(epsilon_est^2) / df)
+
+  ## test statistic
+  CXtX_invCt <- C %*% XtX_inv %*% t(C) # matrix L x L
+  CXtX_diag <- sqrt(diag(CXtX_invCt)) # vecteur L
+  denom <- outer(CXtX_diag, sigma_est, "*") # matrix L x D
+  stat_test <- C %*% beta_est / denom # matrix (L x D)
+
+  ## p-values
+  pvaleurs <- 2 * pt(-abs(stat_test), df = df)
+
+  return(list(
+    epsilon_est = epsilon_est,
+    stat_test = stat_test,
+    pvalues = pvaleurs,
+    beta_est = beta_est
+  ))
+}
+
+#' Permuted p-values using bootstrap for linear model
+#' @param Y Expression matrix of size $n$ observations (in row) and $D$
+#' dimensions/mesures in columns
+#' @param X Design matrix of size $n$ observations (in row) and $p$ variables
+#' (in columns)
+#' @param C Contrast matrix of size $L$ tested contrast (in row) and $p$ columns
+#' corresponding to te tested parameters
+#' @param B scalar, number of bootstrap
+#' @param replace Bool. If TRUE (default) then the residuals are sampled with
+#' replacement (i.e. a bootstrap), if FALSE then they are sampled without
+#' replacement resulting in a permutation of the data
+#'
+#' @return matrix of permuted $p$-values. It is an array of dimensions 
+#' \eqn{D \times L \times B}.
+#' @export
+#'
+#' @examples
+#' N = 100
+#' p = 2
+#' D = 2
+#' X <- matrix(0,nrow = N, ncol = p)
+#' X[,1] <- 1
+#' X[,-1] <- runif(N*(p-1), min = 0, max = 3)
+#' beta <- matrix(0, nrow = p, ncol = D)
+#' epsilons <- matrix(rnorm(N*D), nrow = N, ncol = D)
+#' Y <- X %*% beta + epsilons
+#' C <- diag(p)
+#' resLM <- bootstrap_permutation(Y = Y, X = X, C = C, B = 10)
+bootstrap_permutation <- function(Y, X, C, B = 1000, replace = TRUE) {
+  set.seed(13012024)
+  .check_lm_test(Y, X, C)
+  if (!is.numeric(B) || B < 0) {
+    stop("B must be a positive numeric value",
+      call. = FALSE
+    )
+  }
+
+  n <- nrow(Y)
+  D <- ncol(Y)
+  L <- nrow(C)
+
+  ## perform LM test to obtain esiduals
+  resLM <- lm_test(Y = Y, X = X, C = C)
+  epsilon_hat <- resLM$epsilon_est
+
+  ## Bootstrapping of residuals
+  epsilon_perm <- array(NA, dim = c(dim(epsilon_hat), B))
+  for (b in 1:B) {
+    shuffle_idx <- sample.int(n, replace = replace)
+    epsilon_perm[, , b] <- epsilon_hat[shuffle_idx, ]
+  }
+  # Xhatbeta <- X %*% resLM$beta_est
+
+  ## computation of permuted expression Y_n^{(b)}
+  Y_perm <- epsilon_perm
+  # Y_perm <- sweep(epsilon_perm, 1:2, Xhatbeta, "+")
+
+  ## Test contrasts for each permuted expression matrix
+  pval_perm <- array(NA, dim = c(D, L, B))
+  for (b in 1:B) {
+    pval_perm[, , b] <- lm_test(
+      Y = as.matrix(Y_perm[, , b]),
+      X = X, C = C
+    )$pvalues
+  }
+
+  return(pval_perm)
+}
+
+#' Calibration of post hoc bound using bootstrap permutations
+#'
+#' @param Y Expression matrix of size $n$ observations (in row) and $D$
+#' dimensions/mesures in columns
+#' @param X Design matrix of size $n$ observations (in row) and $p$ variables
+#' (in columns)
+#' @param C Contrast matrix of size $L$ tested contrast (in row) and $p$ columns
+#' corresponding to te tested parameters
+#' @param B scalar, number of bootstrap
+#'
+#' @return A list with elements
+#' - thr: A numeric vector of length K, such that the estimated probability that
+#' there exists an index k between 1 and K such that the k-th maximum of the
+#' test statistics of is greater than `thr[k]`, is less than alpha
+#' - piv_stat: A vector of `B` pivotal statitics
+#' - lambda: A numeric value, the result of the calibration
+#' @export
+#'
+#' @examples
+#' N = 100
+#' p = 2
+#' D = 2
+#' X <- matrix(0,nrow = N, ncol = p)
+#' X[,1] <- 1
+#' X[,-1] <- runif(N*(p-1), min = 0, max = 3)
+#' beta <- matrix(0, nrow = p, ncol = D)
+#' epsilons <- matrix(rnorm(N*D), nrow = N, ncol = D)
+#' Y <- X %*% beta + epsilons
+#' C <- diag(p)
+#' resLM <- calibration_bootstap(Y = Y, X = X, C = C, B = 10)
+calibration_bootstap <- function(Y, X, C, B = 1000) {
+  .check_lm_test(Y, X, C)
+  if (!is.numeric(B) || B < 0) {
+    stop("B must be a positive numeric value",
+      call. = FALSE
+    )
+  }
+
+  D <- dim(Y)[2]
+  L <- dim(C)[1]
+
+  ## perform permuted pvalues
+  pval_perm <- bootstrap_permutation(Y = Y, X = X, C = C, B = B, replace = TRUE)
+
+  ## transform 3 dimensional problem (D, L, B) into a matrix (D*L, B)
+  pval_perm_martix <- matrix(pval_perm, nrow = D * L, ncol = B)
+
+  ## The next steps of the calibration are already implemented in sanssouci
+  return(sanssouci::calibrate0(pval_perm_martix,
+    m = D * L, alpha = 0.05,
+    family = "Linear"
+  ))
+}
+
+.check_lm_test <- function(Y, X, C) {
+  vars <- list(Y, X, C)
+  names <- as.character(substitute(list(Y, X, C)))[-1]
+  for (i in seq_along(vars)) {
+    if (!is.matrix(vars[[i]])) {
+      stop(sprintf("'%s' must be a matrix", names[i]), call. = FALSE)
+    }
+  }
+
+  if (nrow(Y) != nrow(X)) {
+    stop("nrow(Y) must be equal to nrow(X). This is
+                              corresponding to $n$ the number of observations",
+      call. = FALSE
+    )
+  }
+  if (ncol(X) != ncol(C)) {
+    stop("ncol(X) must be equal to ncol(C). This is
+                              corresponding to $p$ the number of variables",
+      call. = FALSE
+    )
+  }
+  invisible(TRUE)
+}
---
+++ tests/testthat/test_bootstrapCalibration.R
@@ -0,0 +1,187 @@
+library(testthat)
+library(reticulate)
+
+test_that('Consistency of `lm_test`', {
+  
+  ## If X is continuous
+  DD <- c(1,10)
+  PP <- c(2,10)
+  N = 100
+  for (D in DD){
+    for (p in PP){
+      X <- matrix(0,nrow = N, ncol = p)
+      X[,1] <- 1
+      X[,-1] <- runif(N*(p-1), min = 0, max = 3)
+      beta <- matrix(0, nrow = p, ncol = D)
+      epsilons <- matrix(rnorm(N*D), nrow = N, ncol = D)
+      Y <- X %*% beta + epsilons
+      C <- diag(p)
+      
+      resLM <- lm_test(Y = Y, X = X, C = C)
+      
+      expect_type(resLM, "list")
+      expect_true("epsilon_est" %in% names(resLM))
+      expect_true("stat_test" %in% names(resLM))
+      expect_true("pvalues" %in% names(resLM))
+      expect_true("beta_est" %in% names(resLM))
+      expect_equal(dim(resLM$epsilon_est), c(nrow(Y), ncol(Y)))
+      expect_equal(dim(resLM$stat_test), c(nrow(C), ncol(Y)))
+      expect_equal(dim(resLM$pvalues), c(nrow(C), ncol(Y)))
+      expect_equal(dim(resLM$beta_est), c(ncol(X), ncol(Y)))
+      
+      for (j in 1:ncol(Y)) {
+        fit <- lm(Y[, j] ~ X[, -1])  
+        s <- summary(fit)
+        p_lm <- unname(coef(s)[, 4])  # p-value
+        expect_equal(resLM$pvalues[, j], p_lm, tolerance = 1e-6)
+        stat_lm <- unname(coef(s)[, 3])  # stat
+        expect_equal(resLM$stat_test[, j], stat_lm, tolerance = 1e-6)
+        beta_lm <- unname(coef(s)[,"Estimate"]) #estiamted beta
+        expect_equal(resLM$beta_est[, j], beta_lm, tolerance = 1e-6)
+      }
+    }
+  }
+  
+  ## If X is qualitative
+  N = 100
+  for (D in DD){
+    for (p in PP){
+      X <- matrix(0,nrow = N, ncol = p)
+      X[,1] <- 1
+      X[,-1] <- sample(0:1, N*(p-1), replace = TRUE)
+      beta <- matrix(0, nrow = p, ncol = D)
+      epsilons <- matrix(rnorm(N*D), nrow = N, ncol = D)
+      Y <- X %*% beta + epsilons
+      C <- diag(p)
+      
+      resLM <- lm_test(Y = Y, X = X, C = C)
+      
+      expect_type(resLM, "list")
+      expect_true("epsilon_est" %in% names(resLM))
+      expect_true("stat_test" %in% names(resLM))
+      expect_true("pvalues" %in% names(resLM))
+      expect_true("beta_est" %in% names(resLM))
+      expect_equal(dim(resLM$epsilon_est), c(nrow(Y), ncol(Y)))
+      expect_equal(dim(resLM$stat_test), c(nrow(C), ncol(Y)))
+      expect_equal(dim(resLM$pvalues), c(nrow(C), ncol(Y)))
+      expect_equal(dim(resLM$beta_est), c(ncol(X), ncol(Y)))
+      
+      for (j in 1:ncol(Y)) {
+        fit <- lm(Y[, j] ~ X[, -1])  
+        s <- summary(fit)
+        p_lm <- unname(coef(s)[, 4])  # p-value 
+        expect_equal(resLM$pvalues[, j], p_lm, tolerance = 1e-6)
+        stat_lm <- unname(coef(s)[, 3])  # stat
+        expect_equal(resLM$stat_test[, j], stat_lm, tolerance = 1e-6)
+        beta_lm <- unname(coef(s)[,"Estimate"]) #estimated beta
+        expect_equal(resLM$beta_est[, j], beta_lm, tolerance = 1e-6)
+      }
+    }
+  }
+  
+  
+  ## Error class of object
+  N = 10
+  P = 2
+  D = 2
+  L = 1
+  X <- matrix(0,nrow = N, ncol = p)
+  Y <- matrix(1, nrow = N, ncol = D)
+  C <- matrix(1, nrow = L, ncol = D)
+  error_list <- list("a", c("a","b"), 1, 1:3, list("a","b"), array(0, dim = c(2,3,2)))
+  for(error in error_list){
+    expect_error(lm_test(Y = error, X = X, C = C), 
+                 regexp = "'Y' must be a matrix")
+    expect_error(lm_test(Y = Y, X = error, C = C),
+                 regexp = "'X' must be a matrix")
+    expect_error(lm_test(Y = Y, X = X, C = error),
+                 regexp = "'C' must be a matrix")
+  }
+  
+  #test missmatch number of observation
+  X <- matrix(0,nrow = N, ncol = p)
+  Y <- matrix(1, nrow = N+1, ncol = D)
+  C <- matrix(1, nrow = L, ncol = D)
+  expect_error(lm_test(Y = Y, X = X, C = C))
+  
+  #test missmatch number of variables
+  X <- matrix(0,nrow = N, ncol = p)
+  Y <- matrix(1, nrow = N, ncol = D)
+  C <- matrix(1, nrow = L, ncol = D+1)
+  expect_error(lm_test(Y = Y, X = X, C = C))
+})
+
+
+test_that("`bootstrap_permutation`", {
+  
+  DD <- c(1,10)
+  PP <- c(2,10)
+  BB <- c(10, 100)
+  N = 100
+  for (D in DD){
+    for (p in PP){
+      for(B in BB) {
+        
+        X <- matrix(0,nrow = N, ncol = p)
+        X[,1] <- 1
+        X[,-1] <- runif(N*(p-1), min = 0, max = 3)
+        beta <- matrix(0, nrow = p, ncol = D)
+        epsilons <- matrix(rnorm(N*D), nrow = N, ncol = D)
+        Y <- X %*% beta + epsilons
+        C <- diag(p)
+        
+        pval_perm <- bootstrap_permutation(Y = Y, X = X, C = C, B, 
+                                           replace = TRUE )
+        
+        expect_equal(class(pval_perm), "array")
+        expect_equal(dim(pval_perm), c(ncol(Y), nrow(C), B ))
+        expect_true(all(pval_perm <= 1))
+        expect_true(all(pval_perm >= 0))
+      }
+    }
+  }
+  
+  ## Error class of object
+  N = 10
+  P = 2
+  D = 2
+  L = 1
+  B = 10
+  X <- matrix(0,nrow = N, ncol = p)
+  Y <- matrix(1, nrow = N, ncol = D)
+  C <- matrix(1, nrow = L, ncol = D)
+  error_list <- list("a", c("a","b"), 1, 1:3, list("a","b"), array(0, dim = c(2,3,2)))
+  for(error in error_list){
+    expect_error(bootstrap_permutation(Y = error, X = X, C = C, B = B), 
+                 regexp = "'Y' must be a matrix")
+    expect_error(bootstrap_permutation(Y = Y, X = error, C = C, B =B),
+                 regexp = "'X' must be a matrix")
+    expect_error(bootstrap_permutation(Y = Y, X = X, C = error, B = B),
+                 regexp = "'C' must be a matrix")
+  }
+  
+  #test missmatch number of observation
+  X <- matrix(0,nrow = N, ncol = p)
+  Y <- matrix(1, nrow = N+1, ncol = D)
+  C <- matrix(1, nrow = L, ncol = D)
+  expect_error(bootstrap_permutation(Y = Y, X = X, C = C, B = B))
+  
+  #test missmatch number of variables
+  X <- matrix(0,nrow = N, ncol = p)
+  Y <- matrix(1, nrow = N, ncol = D)
+  C <- matrix(1, nrow = L, ncol = D+1)
+  expect_error(bootstrap_permutation(Y = Y, X = X, C = C, B = B))
+  
+  #test values of B
+  X <- matrix(0,nrow = N, ncol = p)
+  Y <- matrix(1, nrow = N, ncol = D)
+  C <- matrix(1, nrow = L, ncol = D)
+  expect_error(bootstrap_permutation(Y = Y, X = X, C = C, B = "a"))
+  expect_error(bootstrap_permutation(Y = Y, X = X, C = C, B = matrix(1, 
+                                                                     nrow = 2, 
+                                                                     ncol = 3)))
+  expect_error(bootstrap_permutation(Y = Y, X = X, C = C, B = list("a")))
+  expect_error(bootstrap_permutation(Y = Y, X = X, C = C, B = -5))
+})
+
+
